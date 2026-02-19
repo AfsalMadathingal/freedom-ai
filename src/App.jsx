c662ad756f8faa9e8a50f460f5da4d705f792cfb
afsal <madathingalafsal@gmail.com>
2026-02-19 17:29:04 +0530

    fix: pages support

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Chat from "./chat.jsx";
 import Home from "./home.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
 function App() {
   const [isPinned, setIsPinned] = useState(false);
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <Home isPinned={isPinned} setIsPinned={setIsPinned} />,
